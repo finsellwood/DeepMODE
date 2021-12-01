ac5cee841564a98450f87b626aa9a592574c7905
finsellwood <57958660+finsellwood@users.noreply.github.com>
2021-12-01 19:38:42 +0000

    Create gpu_template.sh
    created a template for any further training on gpus

---
+++ gpu_template.sh
@@ -0,0 +1,12 @@
+#$-q gpu.q@lxcgpu* -l h_rt=24:00:00
+#$-m ea -M fjo18@ic.ac.uk
+
+cd ~/CMSSW_10_2_19/src/UserCode/DeepLearning
+export PYTHONPATH=/home/hep/fjo18/.local/lib/python3.6/site-packages:$PYTHONPATH
+conda activate icenet
+source ~/CMSSW_10_2_19/icenet/setenv.sh
+source /vols/grid/cms/setup.sh
+
+eval `scramv1 runtime -sh`
+
+python3 Python_files/FILENAME.py
