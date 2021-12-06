cfe1f6c6bc6ac4439925932ebb871cbbbdd45d13
finsellwood <57958660+finsellwood@users.noreply.github.com>
2021-12-06 19:34:01 +0000

    Update dataframe_debug_fin.py

--- Python_files/dataframe_debug_fin.py
+++ Python_files/dataframe_debug_fin.py
@@ -57,7 +57,7 @@ for index, row in df_ordered.iterrows():
                        "py": row[fourmom_list[2]].values.tolist(),\
                        "pz": row[fourmom_list[3]].values.tolist(),\
                       })
-    df_ordered["newline"].loc[index] = fourvect.px
+    df_ordered["newline"].loc[[index]] = fourvect.px
 time_elapsed = time.time() - time_start
 print("elapsed time = " + str(time_elapsed)) 
 
