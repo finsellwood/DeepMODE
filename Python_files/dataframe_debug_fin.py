95972d368529621c2f9c0d9a3d51fb6980035355
finsellwood <57958660+finsellwood@users.noreply.github.com>
2021-12-06 19:33:25 +0000

    Update dataframe_debug_fin.py

--- Python_files/dataframe_debug_fin.py
+++ Python_files/dataframe_debug_fin.py
@@ -53,11 +53,11 @@ time_start = time.time()
 # 
 df_ordered["newline"] = [[]] * df_ordered.shape[0]
 for index, row in df_ordered.iterrows():
-  fourvect = vector.arr({"px": row[fourmom_list[1]].values.tolist(),\
+    fourvect = vector.arr({"px": row[fourmom_list[1]].values.tolist(),\
                        "py": row[fourmom_list[2]].values.tolist(),\
                        "pz": row[fourmom_list[3]].values.tolist(),\
                       })
-  print(df_ordered["newline"].loc[index]) = fourvect.px
+    df_ordered["newline"].loc[index] = fourvect.px
 time_elapsed = time.time() - time_start
 print("elapsed time = " + str(time_elapsed)) 
 
