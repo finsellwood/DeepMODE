21fa444097c26431c65aa3d7de707a7463633285
finsellwood <57958660+finsellwood@users.noreply.github.com>
2021-12-06 21:40:49 +0000

    Update dataframe_debug_fin.py

--- Python_files/dataframe_debug_fin.py
+++ Python_files/dataframe_debug_fin.py
@@ -99,12 +99,13 @@ def phi_eta_find(dataframe):
                                py = dataframe[tau_2_4mom[2]],\
                                pz = dataframe[tau_2_4mom[3]],\
                                E = dataframe[tau_2_4mom[0]])
+    print(fourvect.deltaphi(tauvisfourvect))
 #     for a in range(20):
 #         print(fourvect.x[a], 'x')
 #         print(fourvect.y[a], 'y')
 #         print(fourvect.z[a], 'z')
 #         print(fourvect.t[a], 't')
-      print(fourvect.deltaphi(tauvisfourvect))
+      
 #     output_dataframe["phis"] = fourvect.deltaphi(tauvisfourvect) 
 #     output_dataframe["etas"] = fourvect.deltaeta(tauvisfourvect) 
 #     output_dataframe["frac_energies"] = fourvect.E/tauvisfourvect.E
