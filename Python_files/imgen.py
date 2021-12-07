d308e65530428a84c62caae06eccd5b32f0e5ab1
finsellwood <57958660+finsellwood@users.noreply.github.com>
2021-12-07 13:42:11 +0000

    Update imgen.py

--- Python_files/imgen.py
+++ Python_files/imgen.py
@@ -13,13 +13,13 @@ import pylab as pl
 
 #~~ Load the dataframe with image variables in ~~#
 imvar_df = joblib.load(rootpath + "/Objects/imvar_df.sav")
-phis = imvar_df['phis'].to_numpy(dtype = 'float16').flatten()
-etas = imvar_df['etas'].to_numpy(dtype = 'float16').flatten()
-energies = imvar_df['frac_energies'].to_numpy().flatten()
-phis = np.array([phis]) * np.array([energies])
-etas = np.array([etas]) * np.array([energies])
-print(max(phis), max(etas))
-
+phis = np.array(imvar_df['phis'].to_numpy().flatten(), dtype = 'float16')
+etas = np.array(imvar_df['etas'].to_numpy().flatten(), dtype = 'float16')
+energies = np.array(imvar_df['frac_energies'].to_numpy().flatten(), dtype = 'float16')
+# phis = np.array([phis]) * np.array([energies])
+# etas = np.array([etas]) * np.array([energies])
+# print(max(phis), max(etas))
+print(phis)
 
 #~~ Function to generate images ~~#
 def largegrid(dataframe, dimension_l, dimension_s):
